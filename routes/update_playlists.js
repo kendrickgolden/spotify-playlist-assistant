19ac76112507bdf0ea88904d688b695575f1e6db
kendrickgolden <kendrickgolden24@gmail.com>
2022-07-12 17:51:54 -0500

    update_playlists properly creates map with playlists to be updated, and songs to be added

--- routes/update_playlists.js
+++ routes/update_playlists.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 const callbackRouter = require('./callback');
 const fetch = require('node-fetch');
 const User = require('../models/user');
@@ -9,33 +9,35 @@ router.get('/',function(req, res, next) {
     //TODO: Scan liked songs for artist in saved playlists and update
     const token = callbackRouter.token;
     const user_id = callbackRouter.user_id;
-    //const limit = 50;
-   // var offset = 0;
-   // var total_songs = 1;
-    var artist_map = new Map();
+    let artist_map = new Map();
 
     updateSongs();
 
     async function updateSongs(){
         await getPlaylists();
-        //console.log(artist_map);
-        //await updatePlaylists();
+
+        //TODO: create arrays of songs to be added, and then add in groups
+        await updateMap();
+        
+        //updatePlaylists();
+        console.log(artist_map);
     }
 
-    async function getPlaylists() {
+    function getPlaylists() {
        // return new Promise((res, rej) => {
-            console.log("D");
+            //console.log("D");
             return new Promise (resolve => User.findOne({id: user_id}, async (error,data) => {
                 if(error){
                     console.log("Could not find playlists:" + error);
                 } else {
-                    for(var playlist of data.playlists) {
-                        var playlist_obj = JSON.parse(playlist);
-                        var map_value = {playlist_id: playlist_obj.id, tracklist: []};
-                        await getPlaylistSongs(map_value);
+                    for(let playlist of data.playlists) {
+                        let playlist_obj = JSON.parse(playlist);
+                        let map_value = {playlist_id: playlist_obj.id, tracklist: [], new_tracks: []};
                         artist_map.set(playlist_obj.artist_id,map_value);
+
+                        await getPlaylistSongs(map_value);
                     //  console.log(artist_map);
-                    console.log("A");
+                    //console.log("A");
                     }
                 } 
                 resolve();
@@ -44,13 +46,13 @@ router.get('/',function(req, res, next) {
 
 
     async function getPlaylistSongs(map_value) {
-        var playlist_id = map_value.playlist_id;
-        var tracklist = map_value.tracklist;
-        var offset = 0;
-        var total_songs = 1;
+        let playlist_id = map_value.playlist_id;
+        let tracklist = map_value.tracklist;
+        let offset = 0;
+        let total_songs = 1;
         while(offset < total_songs) {
            // console.log("A");
-            var fetchPromise = fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks?fields=total,items(track.id)&limit=50&offset=${offset}`, {
+            let fetchPromise = fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks?fields=total,items(track.id)&limit=50&offset=${offset}`, {
                 method: 'GET',
                 headers: { 'Authorization' : 'Bearer ' + token}
             });
@@ -63,6 +65,8 @@ router.get('/',function(req, res, next) {
                     return response.json();
                 })
                 .then(data => createSongArray(data, tracklist))
+                .then(data => total_songs = data)
+                .then(offset+=50)
                 .catch(error => {
                     console.error(`Could not get playlist songs: ${error}`);
                 });
@@ -71,15 +75,24 @@ router.get('/',function(req, res, next) {
     }
 
     
-    async function createSongArray(data, tracklist){
-        var song_array = data.items.map(x => x.track.id);
-        tracklist.push(song_array);
-        total_songs = data.total;
+    function createSongArray(data, tracklist){
+        let song_array = data.items.map(x => x.track.id);
+        for(song of song_array) {
+            //console.log(song);
+            tracklist.push(song);
+        }
+        //tracklist = song_array;
+        return data.total;
     }
   
-    async function updatePlaylists() {
+     function updateMap() {
+       return new Promise(async resolve =>  {
+        let offset = 0;
+        let total_songs = 1;
+
         while(offset < total_songs) {
-            var fetchPromise = fetch(`https://api.spotify.com/v1/me/tracks?limit=50&offset=${offset}`, {
+            //console.log("b")
+            let fetchPromise = fetch(`https://api.spotify.com/v1/me/tracks?limit=50&offset=${offset}`, {
                 method: 'GET',
                 headers: { 'Authorization' : 'Bearer ' + token}
             });
@@ -92,38 +105,40 @@ router.get('/',function(req, res, next) {
                     return response.json();
                 })
                 .then(data => addNewSongs(data))
+                .then(data => total_songs = data)
+                .then(offset+=50)
+                //.then(console.log("testt"))
                 .catch(error => {
                     console.error(`Could not get liked_songs: ${error}`);
                 });
-                offset+=50;
-        }
+ 
+            }
+           // console.log("o: " + offset)
+          //  console.log("t: " + total_songs)
+            resolve();
+        });
     }
 
-    //TODO: add new songs
-    async function addNewsongs() {
-        for(var i = 0; i < data.items.length; i++) {
-            var current_track = data.items[i].track;
-            for(var artist of current_track.artists) {
-                if(artist_map.has(artist.id)) {
-                    
-
-                    var current_tracklist = artist_map.get(artist.id)
-                    var new_tracklist = [];
-                    new_tracklist.push(current_track.uri);
-                    artist_map.set(artist.name,new_tracklist);
+     function addNewSongs(data) {
+        for(let i = 0; i < data.items.length; i++) {
+            let current_track = data.items[i].track;
+            for(let artist of current_track.artists) {
+                if(artist_map.has(artist.id) && !artist_map.get(artist.id).tracklist.includes(current_track.id)) {
+                    artist_map.get(artist.id).new_tracks.push(current_track.id);
                 } 
             }
         }
-        console.log("total: " + data.total);
-        total_songs = data.total;
+        //console.log("total: " + data.total);
+        return data.total;
     }
+
   
      /*        const playlists = await User.where('id').equals(user_id).select("playlists").then(query => {
             console.log(JSON.stringify(query.playlists));
         });
-      //  for(var playlist of playlists.playlist) {
+      //  for(let playlist of playlists.playlist) {
         //    console.log("playlist: " + playlist);
-         //   var playlist_obj = JSON.parse(playlist);
+         //   let playlist_obj = JSON.parse(playlist);
         //    console.log("OBJ: " + playlist_obj);
            // artist_map.set(playlist_obj.artist_id,playlist_obj.id);
       //  }
@@ -136,9 +151,9 @@ router.get('/',function(req, res, next) {
                 console.log(query[0].playlists);
             }
             
-               for(var playlist of playlists.playlist) {
+               for(let playlist of playlists.playlist) {
                     console.log("playlist: " + playlist);
-                    var playlist_obj = JSON.parse(playlist);
+                    let playlist_obj = JSON.parse(playlist);
                     console.log("OBJ: " + playlist_obj);
                     artist_map.set(playlist_obj.artist_id,playlist_obj.id);
                 }
@@ -148,7 +163,7 @@ router.get('/',function(req, res, next) {
      //gets all of a user's liked tracks
   /*   async function getLikedTracks(){
         while(offset < total_songs) {
-            var fetchPromise = fetch(`https://api.spotify.com/v1/me/tracks?limit=${limit}&offset=${offset}`, {
+            let fetchPromise = fetch(`https://api.spotify.com/v1/me/tracks?limit=${limit}&offset=${offset}`, {
                 method: 'GET',
                 headers: { 'Authorization' : 'Bearer ' + token}
             });
@@ -171,15 +186,15 @@ router.get('/',function(req, res, next) {
 
     //Creates map of form artist: ["track1_uri,track2_uri,..."]
     function createArtistMap(data) {
-        for(var i = 0; i < data.items.length; i++) {
-            var current_track = data.items[i].track;
-            for(var artist of current_track.artists) {
+        for(let i = 0; i < data.items.length; i++) {
+            let current_track = data.items[i].track;
+            for(let artist of current_track.artists) {
                 if(artist_map.has(artist.id)) {
-                    var new_tracklist = [];
+                    let new_tracklist = [];
                     new_tracklist.push(current_track.uri);
                     artist_map.set(artist.name,new_tracklist);
                 } else {
-                    var exisiting_tracklist = artist_map.get(artist.name);
+                    let exisiting_tracklist = artist_map.get(artist.name);
                     exisiting_tracklist.push(current_track.uri);
                     artist_map.set(artist.name,exisiting_tracklist);
                 }
@@ -193,7 +208,7 @@ router.get('/',function(req, res, next) {
     async function updatePlaylists(songs){
         const playlists = await User.where('id').equals(callbackRouter.user_id).select("playlists");
         for(playlist of playlists) {      
-            var fetchPromise = fetch(`https://api.spotify.com/v1/playlists/${playlist}/tracks`, {
+            let fetchPromise = fetch(`https://api.spotify.com/v1/playlists/${playlist}/tracks`, {
                     method: 'POST',
                     headers: { 'Authorization' : 'Bearer ' + token},
                     body: JSON.stringify({"uris" : value})
